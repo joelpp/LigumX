7341d3fc9390095ef292953e4d6bc61f38cbf066
Adrien D <ra.dubouchet@gmail.com>
2015-05-08 17:49:49 -0400

    Working on realistic driving model with gears and shit.

--- protoEngine/src/entity.cpp
+++ protoEngine/src/entity.cpp
@@ -5,59 +5,148 @@
 #include "glm/gtc/type_ptr.hpp"
 #include "glm/gtx/string_cast.hpp"
 
+#define printvec3(v) std::cout << v.x << " " << v.y << " " << v.z << std::endl
 using namespace glm;
 
 #define ENTITY_SIZE 0.0001f
 #define POWER_MAG ENTITY_SIZE * 10.f
 #define FRICTION_COEFF 0.5f
+#define DEFAULT_ENTITY_TYPE CarEntityType
 
-class BasicVehicleDef {
+class CarDef : public EntityType {
 public:
-    static void FillPositions(vec3 *dataPtr, const Entity &e) {
-        const Body &b = e.GetBody();
+    CarDef()  {
+        segmentCount = 22;
+        mass = 1500.f;
+        maxThrust = 50.f;
+        resistanceDrag = 0.4257f;
+        resistanceRolling = 30.f * resistanceDrag;
+        forwardGears = 6;
+        backwardGears = 1;
+        differentialRatio = 3.42f;
+        wheelRadius = 0.34f;
+
+        gearRatios[0] = 1.f/2.66f;
+        gearRatios[1] = 1.f/1.78f;
+        gearRatios[2] = 1.f/1.30f;
+        gearRatios[3] = 1.f/1.00f;
+        gearRatios[4] = 1.f/0.74f;
+        gearRatios[5] = 1.f/0.50f;
+
+        wheelWidth = 0.075f;
+        wheelHeight = 0.2f;
+    }
+
+    void MakeDataPositions(glm::vec3 *target, const Body &b) {
         const vec3 right = b.right;
         const vec3 fwd = b.forward;
         const vec3 pos = b.position;
 
-        dataPtr[0*2] = pos - 0.5f * ENTITY_SIZE * right;
-        dataPtr[0*2+1] = pos + 0.5f * ENTITY_SIZE * right;
-        dataPtr[1*2] = pos - 0.5f * ENTITY_SIZE * right;
-        dataPtr[1*2+1] = pos + 1.5f * ENTITY_SIZE * fwd;
-        dataPtr[2*2] = pos + 0.5f * ENTITY_SIZE * right;;
-        dataPtr[2*2+1] = pos + 1.5f * ENTITY_SIZE * fwd;
+        target[0*2] = pos - 0.5f * ENTITY_SIZE * right;
+        target[0*2+1] = pos + 0.5f * ENTITY_SIZE * right;
+        target[1*2] = pos - 0.5f * ENTITY_SIZE * right;
+        target[1*2+1] = pos + 1.5f * ENTITY_SIZE * fwd - 0.2f * ENTITY_SIZE * right;
+        target[2*2] = pos + 0.5f * ENTITY_SIZE * right;
+        target[2*2+1] = pos + 1.5f * ENTITY_SIZE * fwd + 0.2f * ENTITY_SIZE * right;
+        target[3*2] = pos + 1.5f * ENTITY_SIZE * fwd - 0.2f * ENTITY_SIZE * right;
+        target[3*2+1] = pos + 1.5f * ENTITY_SIZE * fwd + 0.2f * ENTITY_SIZE * right;
 
         // Create wheels
-        MakeWheelAt(&dataPtr[3*2], pos + 1.5f * ENTITY_SIZE * fwd - 0.5f * ENTITY_SIZE * right, ENTITY_SIZE * fwd, ENTITY_SIZE * right);
-        MakeWheelAt(&dataPtr[7*2], pos + 1.5f * ENTITY_SIZE * fwd + 0.5f * ENTITY_SIZE * right, ENTITY_SIZE * fwd, ENTITY_SIZE * right);
-        MakeWheelAt(&dataPtr[11*2], pos - 0.6f * ENTITY_SIZE * right, ENTITY_SIZE * fwd, ENTITY_SIZE * right);
-        MakeWheelAt(&dataPtr[15*2], pos + 0.6f * ENTITY_SIZE * right, ENTITY_SIZE * fwd, ENTITY_SIZE * right);
+        MakeWheelAt(&target[4*2], pos + 1.5f * ENTITY_SIZE * fwd - 0.5f * ENTITY_SIZE * right, ENTITY_SIZE * fwd, ENTITY_SIZE * right);
+        MakeWheelAt(&target[8*2], pos + 1.5f * ENTITY_SIZE * fwd + 0.5f * ENTITY_SIZE * right, ENTITY_SIZE * fwd, ENTITY_SIZE * right);
+        MakeWheelAt(&target[12*2], pos - 0.6f * ENTITY_SIZE * right, ENTITY_SIZE * fwd, ENTITY_SIZE * right);
+        MakeWheelAt(&target[16*2], pos + 0.6f * ENTITY_SIZE * right, ENTITY_SIZE * fwd, ENTITY_SIZE * right);
 
         // debug visuals
-        dataPtr[(lineCount-2)*2] = pos;
-        dataPtr[(lineCount-2)*2+1] = pos + ENTITY_SIZE * right;
-        dataPtr[(lineCount-1)*2] = pos;
-        dataPtr[(lineCount-1)*2+1] = pos + b.GetForwardVelocity() / 6.f;
+        target[(segmentCount-2)*2] = pos;
+        target[(segmentCount-2)*2+1] = pos + ENTITY_SIZE * right;
+        target[(segmentCount-1)*2] = pos;
+        target[(segmentCount-1)*2+1] = pos + /*ENTITY_SIZE **/ b.GetForwardVelocity();
     }
 
-    static void FillColors(vec3 *dataPtr, const Entity &e) {
-        for(size_t i = 0; i < lineCount-2; ++i) {
-            dataPtr[2*i] = e.color;
-            dataPtr[2*i+1] = e.color;
+    void MakeDataColors(glm::vec3 *target, const Entity &e) {
+        for(size_t i = 0; i < segmentCount-2; ++i) {
+            target[2*i] = e.color;
+            target[2*i+1] = e.color;
         }
 
         // color of debug visuals
-        dataPtr[(lineCount-2)*2] = vec3(1,0,0);
-        dataPtr[(lineCount-2)*2+1] = vec3(1,0,0);
-        dataPtr[(lineCount-1)*2] = vec3(0,1,0);
-        dataPtr[(lineCount-1)*2+1] = vec3(0,1,0);
+        target[(segmentCount-2)*2] = vec3(1,0,0);
+        target[(segmentCount-2)*2+1] = vec3(1,0,0);
+        target[(segmentCount-1)*2] = vec3(0,1,0);
+        target[(segmentCount-1)*2+1] = vec3(0,1,0);
     }
 
-    static size_t LineCount() {
-        return lineCount;
+    void UpdateLogic(float dt, Body &b, int control_state) {
+        float engine_force = 0.f;
+        float air_resistance = 0.f;
+        float rolling_resistance = 0.f;
+
+        vec3 velocity_vector = b.GetForwardVelocity();
+        float curr_speed = length(velocity_vector);
+        vec3 forward_normal = b.ToLocal(vec3(0, 1, 0));
+
+        // Forward thrust power
+        if(control_state & Entity::CS_FWD) {
+            if(!b.speedMult) {
+                b.speedMult = 1;
+                std::cout << b.speedMult << std::endl;
+                b.rpm = 10;
+            }
+            engine_force = maxThrust * ENTITY_SIZE;
+        } else if(control_state & Entity::CS_BWD) {
+            if(!b.speedMult) {
+                b.speedMult = -1;
+                std::cout << b.speedMult << std::endl;
+                b.rpm = 10;
+            }
+            engine_force = -maxThrust * ENTITY_SIZE;
+        }
+
+        b.wheel_acceleration = engine_force * gearRatios[b.speedMult] * differentialRatio;
+
+        // integrate
+        b.wheel_velocity += b.wheel_acceleration * (float)dt;
+
+
+        // Update engine RPM and gear level if necessary
+        b.rpm = b.wheel_velocity * gearRatios[b.speedMult] * differentialRatio * 60 / (2.f * M_PI);
+
+        if(b.speedMult < 0) {
+            if(b.rpm < 100.f) {
+                b.speedMult = 0;
+                std::cout << b.speedMult << std::endl;
+            }
+            // no else, only 1 gear in reverse (TODO : more reverse gear, is that even a thing ??)
+        }
+        else if(b.speedMult > 0) {
+            if(b.rpm < 100.f) {
+                --b.speedMult;
+                std::cout << b.speedMult << std::endl;
+            } else if(b.rpm > gearRatios[b.speedMult] * 3000) {
+                ++b.speedMult;
+                std::cout << b.speedMult << std::endl;
+            }
+        }
+
+
+        float wheel_force = b.wheel_velocity / wheelRadius;
+
+        // Ground friction & lateral force cancellation
+        // TODO : Different ground characteristics
+        if(curr_speed > 0.f) {
+            // F_drag = - C_drag * vel * |vel|
+            air_resistance = -resistanceDrag * curr_speed;
+            // F_rolling = - C_rolling * vel
+            rolling_resistance = -resistanceRolling;
+        }
+
+        // a = \sum(F)/m
+        b.acceleration += (wheel_force * forward_normal + (air_resistance + rolling_resistance) * b.velocity) / mass;
     }
 
 private:
-    static void MakeWheelAt(vec3 *dataPtr, vec3 pos, vec3 fwd, vec3 right) {
+    void MakeWheelAt(vec3 *dataPtr, vec3 pos, vec3 fwd, vec3 right) {
         dataPtr[0*2] = pos - wheelWidth * right + wheelHeight * fwd;
         dataPtr[0*2+1] = pos + wheelWidth * right + wheelHeight * fwd;
         dataPtr[1*2] = pos - wheelWidth * right + wheelHeight * fwd;
@@ -68,35 +157,22 @@ private:
         dataPtr[3*2+1] = pos + wheelWidth * right - wheelHeight * fwd;
     }
 
-    static const size_t lineCount;
-    static const float wheelWidth;
-    static const float wheelHeight;
-    // Entity features
-    static const float maxThrust;                 //!< Maximum thrust acceleration
-    static const float maxForwardSpeed;      //!< Maximum atteignable forward speed
-    static const float maxBackwardSpeed;    //!< Maximum atteignable backward speed
+    float wheelWidth;
+    float wheelHeight;
 };
 
-// Definition of BasicVehicleDef static variables
-const size_t BasicVehicleDef::lineCount = 21;
-const float BasicVehicleDef::wheelWidth = 0.075f;
-const float BasicVehicleDef::wheelHeight = 0.2f;
-const float BasicVehicleDef::maxThrust = 50.f;                 //!< Maximum thrust acceleration
-const float BasicVehicleDef::maxForwardSpeed = 50.f;      //!< Maximum atteignable forward speed
-const float BasicVehicleDef::maxBackwardSpeed = -10.f;
+CarDef CarEntityType;
+
 
 
 // ###################################################
 
-Body::Body() : position(0.f), mass(1), awake(true) {
+Body::Body() : position(0.f), mass(1), speedMult(0), rpm(0), wheel_velocity(0.f), wheel_acceleration(0.f), awake(true) {
     ResetLocalSpace();
 }
 
-void Body::Update(double dt) {
-    // euler integration
-    position += velocity * (float)dt;
-    velocity += acceleration * (float)dt;
-
+void Body::Update(double dt, int control_state) {
+    acceleration = vec3(0.f);
 
 //    float d_angle = dt * 100.f * turning;
 //    if(turning) {
@@ -106,33 +182,19 @@ void Body::Update(double dt) {
 //    forward = normalize(vec3(cosf(glm::radians(angle)), sinf(glm::radians(angle)), 0));
 //    right = normalize(vec3(cosf(glm::radians(angle-90.f)), sinf(glm::radians(angle-90.f)), 0.f));
 
-    vec3 thrust_force(0.f);
-    vec3 friction_force(0.f);
+//    printvec3(velocity_vector);
+//    std::cout << curr_speed << std::endl;
 
-    float curr_speed = GetForwardSpeed();
+    type->UpdateLogic((float)dt, *this, control_state);
 
-    // Forward thrust power
-    if(desiredSpeed != 0.f) {
-        thrust_force = (desiredSpeed > curr_speed ? 1 : -1) * maxThrust * forward * ENTITY_SIZE;
-    }
+    // euler integration
+    velocity += acceleration * (float)dt;
+    position += velocity * (float)dt;
+}
 
-    // Ground friction & lateral force cancellation
-    // TODO : Different ground characteristics
-    if(length(velocity) > 0.f) {
-        // apply friction
-        vec3 lateral_velocity = GetLateralVelocity();
-        vec3 forward_velocity = GetForwardVelocity();
-        float friction_mag = -2.f * 1000.f* POWER_MAG * length(forward_velocity);
-        friction_force = friction_mag * normalize(forward_velocity);
-
-        // apply lateral impulse to remove lateral movement
-        velocity += -lateral_velocity * mass;
-    }
-
-//    std::cout << desiredSpeed << "   " << curr_speed << std::endl;
-
-    // a = \sum(F)/m
-    acceleration = (thrust_force + friction_force) / mass;
+void Body::GenerateLocalSpace() {
+    right = AngleToVector(radians(angle));
+    forward = AngleToVector(radians(angle+90.f));
 }
 
 void Body::ResetLocalSpace() {
@@ -163,8 +225,7 @@ vec3 Body::GetLateralVelocity() const {
 
 // ###################################################
 
-Entity::Entity() :
-                       angle(0.f), turning(0), desiredSpeed(0.f) {
+Entity::Entity() : color(1), controller(CONTROLLER_AI), controlState(0), type(&DEFAULT_ENTITY_TYPE)  {
 
 }
 
@@ -176,7 +237,11 @@ void Entity::Update(double dt) {
 
 // #############################################
 
-EntityManager::EntityManager() : array_modification(false) {
+EntityDesc::EntityDesc() : position(0), angle(0.f), color(1.f), controller(Entity::CONTROLLER_AI), type(&DEFAULT_ENTITY_TYPE) {
+
+}
+
+EntityManager::EntityManager() : array_modification(false), dataSize(0), filledSize(0) {
 
 }
 
@@ -220,7 +285,7 @@ bool EntityManager::Init() {
     pPipelineEntities->useFragmentShader(pFragmentShader);
 
     // Fixed starting size for data arrays
-    dataSize = 100;
+    dataSize = 256;
     srand(time(NULL));
 
 
@@ -233,47 +298,67 @@ bool EntityManager::Init() {
     glVertexArrayVertexBuffer(pPipelineEntities->glidVao, 1, glidEntitiesColors, 0, sizeof(vec3));
     glVertexArrayAttribFormat(pPipelineEntities->glidVao, 1, 3, GL_FLOAT, GL_FALSE, 0);
 
-    Entity e;
+    EntityDesc e;
     e.position = vec3(-5 * ENTITY_SIZE,0,1);
+    e.angle = 0.f;
     e.color = vec3(1,0.8,0);
-    e.type = Entity::CONTROLLER_PLAYER;
-    e.mass = 1;
+    e.type = &CarEntityType;
+    e.controller = Entity::CONTROLLER_PLAYER;
     AddEntity(e);
 
-    for(size_t i = 0; i < 20; ++i) {
-        Entity e;
-        e.position = vec3((rand01() * 40 - 20)  * ENTITY_SIZE, (rand01() * 40 - 20)  * ENTITY_SIZE, 1);
-        e.angle = rand01() * 360;
-        e.color = vec3(rand01() * 0.7 + 0.3, rand01() * 0.7 + 0.3, rand01() * 0.7 + 0.3);
-        e.type = Entity::CONTROLLER_AI;
-        e.mass = 1;
-        AddEntity(e);
-    }
+//    for(size_t i = 0; i < 20; ++i) {
+//        Entity e;
+//        e.position = vec3((rand01() * 40 - 20)  * ENTITY_SIZE, (rand01() * 40 - 20)  * ENTITY_SIZE, 1);
+//        e.angle = rand01() * 360;
+//        e.color = vec3(rand01() * 0.7 + 0.3, rand01() * 0.7 + 0.3, rand01() * 0.7 + 0.3);
+//        e.type = Entity::CONTROLLER_AI;
+//        e.mass = 1;
+//        AddEntity(e);
+//    }
 
     return true;
 }
 
-void EntityManager::AddEntity(const Entity &entity) {
-    Entity e(entity);
+void EntityManager::AddEntity(const EntityDesc &entity_desc) {
+    size_t ei = entities.size();
 
-    e.forwardVector = normalize(vec3(cosf(glm::radians(e.angle)), sinf(glm::radians(e.angle)), 0));
-    e.rightVector = normalize(vec3(cosf(glm::radians(e.angle-90.f)), sinf(glm::radians(e.angle-90.f)), 0));
-    e.entityIndex = entities.size();
+    size_t dataIndex = filledSize;
+    filledSize += entity_desc.type->segmentCount * 2; // 2 vertices per segment
 
-    // resize arrays/vbos if we get too much entities
-    if(entities.size() > dataSize) {
-        dataSize *= 2;
-        makeVBO();
+    // Make sure we have enough space for secondary arrays
+    if(filledSize >= dataSize) {
+        while(filledSize >= dataSize) {
+            dataSize *= 2;
+            makeVBO();
+        }
     }
 
+    // add entity-body pair to storage
+    entities.push_back(Entity());
+    bodies.push_back(Body());
 
-    BasicVehicleDef::FillPositions(&entityPositions[e.entityIndex*BasicVehicleDef::LineCount()*2], e);
-    BasicVehicleDef::FillColors(&entityColors[e.entityIndex*BasicVehicleDef::LineCount()*2], e);
-    entities.push_back(e);
+    // Initialize the pair
+    Entity &e = GetEntity(ei);
+    Body &b = GetBody(ei);
 
+    e.entityIndex = ei;
+    e.color = entity_desc.color;
+    e.type = entity_desc.type;
+    e.controller = entity_desc.controller;
+    e.dataIndex = dataIndex;
+
+    b.angle = entity_desc.angle;
+    b.position = entity_desc.position;
+    b.type = entity_desc.type;
+    b.mass = b.type->mass;
+    b.GenerateLocalSpace();
+
+    // Generate mesh
+    e.type->MakeDataPositions(&entityPositions[e.dataIndex], b);
+    e.type->MakeDataColors(&entityColors[e.dataIndex], e);
 
     // Controller type
-    if(e.type == Entity::CONTROLLER_PLAYER) {
+    if(e.controller == Entity::CONTROLLER_PLAYER) {
         playerController = PlayerController(this, e.entityIndex);
     } else {
         aiControllers.push_back(AIController(this, e.entityIndex));
@@ -281,8 +366,8 @@ void EntityManager::AddEntity(const Entity &entity) {
 }
 
 void EntityManager::makeVBO() {
-    entityPositions.resize(dataSize * BasicVehicleDef::LineCount());
-    entityColors.resize(dataSize * BasicVehicleDef::LineCount());
+    entityPositions.resize(dataSize);
+    entityColors.resize(dataSize);
 
     glDeleteBuffers(1, &glidEntitiesPositions);
     glCreateBuffers(1, &glidEntitiesPositions);
@@ -308,25 +393,26 @@ void EntityManager::KeyCallback(int key, int action) {
 void EntityManager::Update(double dt) {
     for(size_t i = 0; i < entities.size(); ++i) {
         Entity &e = entities[i];
+        Body &b = bodies[i];
 
         e.Update(dt);
-
+        b.Update(dt, e.controlState);
 
         // TODO : Find when an entity's rendering data shouldnt be updated
-        BasicVehicleDef::FillPositions(&entityPositions[e.entityIndex*BasicVehicleDef::LineCount()*2], e);
+        e.type->MakeDataPositions(&entityPositions[e.dataIndex], b);
         array_modification = true;
     }
 }
 
 void EntityManager::Render(const mat4 &viewMatrix) {
     if(array_modification) {
-        glNamedBufferSubData(glidEntitiesPositions, 0, (entities.size() * BasicVehicleDef::LineCount() * 2) * sizeof(vec3), entityPositions.data());
-        glNamedBufferSubData(glidEntitiesColors, 0, (entities.size() * BasicVehicleDef::LineCount() * 2) * sizeof(vec3), entityColors.data());
+        glNamedBufferSubData(glidEntitiesPositions, 0, filledSize * sizeof(vec3), entityPositions.data());
+        glNamedBufferSubData(glidEntitiesColors, 0, filledSize * sizeof(vec3), entityColors.data());
     }
 
     pPipelineEntities->usePipeline();
     glProgramUniformMatrix4fv(pPipelineEntities->getShader(GL_VERTEX_SHADER)->glidShaderProgram, glGetUniformLocation(pPipelineEntities->getShader(GL_VERTEX_SHADER)->glidShaderProgram, "vpMat"), 1, false, value_ptr(viewMatrix));
-    glDrawArrays(GL_LINES, 0, entities.size() * BasicVehicleDef::LineCount() * 2);
+    glDrawArrays(GL_LINES, 0, filledSize);
 }
 
 
@@ -338,31 +424,29 @@ void PlayerController::Update() {
 void PlayerController::OnKey(int key, int action) {
     assert(entityIndex >= 0);
     Entity &e = entityManager->GetEntity(entityIndex);
+    Body &b = entityManager->GetBody(entityIndex);
 
     if(action == GLFW_PRESS){
-        if(key == GLFW_KEY_KP_4) { e.turning = 1; }
-        if(key == GLFW_KEY_KP_6) { e.turning = -1;}
+        if(key == GLFW_KEY_KP_4) { e.controlState |= Entity::CS_LEFT; }
+        if(key == GLFW_KEY_KP_6) { e.controlState |= Entity::CS_RIGHT; }
 
-        if(key == GLFW_KEY_KP_8) { e.desiredSpeed = ENTITY_SIZE * e.maxForwardSpeed;}
-        if(key == GLFW_KEY_KP_5) { e.desiredSpeed = ENTITY_SIZE * e.maxBackwardSpeed;}
+        if(key == GLFW_KEY_KP_8) { e.controlState |= Entity::CS_FWD; }
+        if(key == GLFW_KEY_KP_5) { e.controlState |= Entity::CS_BWD; }
 
         if(key == GLFW_KEY_F5) {
-            e.position = vec3(-5 * ENTITY_SIZE,0,1);
-            e.acceleration = e.velocity = vec3(0,0,0);
-            e.angle = e.desiredSpeed = 0.f;
-            e.turning = 0;
+//            e.position = vec3(-5 * ENTITY_SIZE,0,1);
+//            e.acceleration = e.velocity = vec3(0,0,0);
+//            e.angle = e.desiredSpeed = 0.f;
+//            e.turning = 0;
         }
     }
 
     if(action == GLFW_RELEASE) {
-        if((key == GLFW_KEY_KP_4 && e.turning == 1) ||
-                (key == GLFW_KEY_KP_6 && e.turning == -1)) {
-            e.turning = 0;
-        }
-        if((key == GLFW_KEY_KP_8 && e.desiredSpeed > 0.f) ||
-                (key == GLFW_KEY_KP_5 && e.desiredSpeed < 0.f)) {
-            e.desiredSpeed = 0.f;
-        }
+        if(key == GLFW_KEY_KP_4) { e.controlState &= ~Entity::CS_LEFT; }
+        if(key == GLFW_KEY_KP_6) { e.controlState &= ~Entity::CS_RIGHT; }
+
+        if(key == GLFW_KEY_KP_8) { e.controlState &= ~Entity::CS_FWD; }
+        if(key == GLFW_KEY_KP_5) { e.controlState &= ~Entity::CS_BWD; }
     }
 }
 
--- protoEngine/src/entity.h
+++ protoEngine/src/entity.h
@@ -7,12 +7,35 @@
 #include "program_pipeline.h"
 
 class EntityManager;
+class EntityType;
+
+
+
+// ####################################
+
+
+/// Get a Unit direction vector from an angle in radians
+inline glm::vec3 AngleToVector(float angle) {
+    return glm::vec3(std::cos(angle), std::sin(angle), 0.f);
+}
+
+/// Get a scalar angle value in radians from a direction vector
+inline float VectorToAngle(const glm::vec3 &v) {
+    return (float)std::atan2(v.y, v.x);
+}
 
 struct Body {
+friend class EntityManager;
+friend class PlayerController;
+friend class AIController;
     Body();
 
-    void Update(double dt);
+    void Update(double dt, int control_state);
 
+    /// Regenerate the body local space from the angle
+    void GenerateLocalSpace();
+
+    /// Reset the body local space to
     void ResetLocalSpace();
     glm::vec3 ToLocal(glm::vec3 global_vector) const;
     glm::vec3 ToGlobal(glm::vec3 local_vector) const;
@@ -30,11 +53,20 @@ struct Body {
 
     float mass;
     float angle;
-    int turning;
-    float desiredSpeed;
+
+    // TODO : differentiate bodies between types ?? (pedestrian, 4wheeled, plane, etc)
+    int speedMult;      //!< In a car, the current gear level. for character, the run level, etc..
+    int rpm;
+    float wheel_velocity;
+    float wheel_acceleration;
 
     bool awake;
+
+private:
+    size_t bodyIndex;
+    EntityType *type;
 };
+
 
 class Entity {
 friend class EntityManager;
@@ -46,31 +78,33 @@ public :
         CONTROLLER_AI
     };
 
+    enum ControlState {
+        CS_FWD = 1 << 1,
+        CS_BWD = 1 << 2,
+        CS_LEFT = 1 << 3,
+        CS_RIGHT = 1 << 4,
+        CS_NONE = 1 << 5
+    };
+
 public:
     Entity();
 
     // @return : true if the entity has been updated
     void Update(double dt);
 
-    Body &GetBody() { return body; }
-    const Body &GetBody() const { return body; }
+//    Body &GetBody() { return body; }
+//    const Body &GetBody() const { return body; }
 
 
 public:
-    // Entity features
-    float maxThrust;                 //!< Maximum thrust acceleration
-    float maxForwardSpeed;      //!< Maximum atteignable forward speed
-    float maxBackwardSpeed;    //!< Maximum atteignable backward speed
-
     glm::vec3 color;
-    ControllerType type;
-
+    ControllerType controller;
+    int controlState;
 
 private:
     size_t entityIndex;
-
-    Body body;
-
+    size_t dataIndex;
+    EntityType *type;
 };
 
 // ####################################
@@ -105,6 +139,44 @@ public:
 
 // ####################################
 
+/// Helper class describing a type of entity in particular (e.g. pedestrian, car)
+/// Inherit from it to create a new type of entity
+class EntityType {
+public:
+    virtual void MakeDataPositions(glm::vec3 *target, const Body &b) { assert(0); }
+    virtual void MakeDataColors(glm::vec3 *target, const Entity &e) { assert(0); }
+
+    /// Update the logic for the given body, according to the derived Entity Type
+    virtual void UpdateLogic(float dt, Body &b, int control_state) { assert(0); }
+
+    size_t segmentCount;         //!< Number of segments for drawing the entity
+
+    // Physic
+    float mass;                       //!< Body mass
+    float maxThrust;                //!< Engine force output, altered by gear level to compute speed
+
+    // Car specific
+    int forwardGears;               //!< Number of gears for forward propulsion
+    int backwardGears;            //!< Number of gears for backward propulsion
+    float gearRatios[7];            //!< Values of gear ratios for each gear. TMP : max=7 gears
+    float differentialRatio;
+    float wheelRadius;
+    float resistanceDrag;
+    float resistanceRolling;
+};
+
+// ####################################
+
+struct EntityDesc {
+    EntityDesc();
+    glm::vec3 position;
+    float angle;
+    glm::vec3 color;
+    EntityType *type;
+    Entity::ControllerType controller;
+};
+
+
 class EntityManager {
 public:
     EntityManager();
@@ -114,13 +186,16 @@ public:
     void Update(double dt);
     void Render(const glm::mat4 &viewMatrix);
 
-    void AddEntity(const Entity &e);
+    void AddEntity(const EntityDesc &entity_desc);
 
+    Body &GetBody(size_t index) { assert(index < bodies.size()); return bodies[index]; }
     Entity &GetEntity(size_t index) { assert(index < entities.size()); return entities[index]; }
     size_t GetEntityCount() const { return entities.size(); }
 
 private:
+    /// Entities & Bodies have a 1:1 relationship. Body at index i is used by entity at index i.
     std::vector<Entity> entities;
+    std::vector<Body> bodies;
 
     PlayerController playerController;
     std::vector<AIController> aiControllers;
@@ -128,7 +203,9 @@ private:
     // internal rendering stuff
     ProgramPipeline* pPipelineEntities;
     bool array_modification;
+
     size_t dataSize;
+    size_t filledSize;
 
     std::vector<glm::vec3> entityPositions;
     GLuint glidEntitiesPositions;
